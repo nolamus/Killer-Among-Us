9f310d987d1573e22a3c99f575e1e860b3f2abeb
Joseph Ene <19315036+nolamus@users.noreply.github.com>
2020-06-04 17:47:46 -0400

    Added Scenes to Project
    Added the Main menu and level 0 scene to the project

---
+++ Assets/GravityOn_Off.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityOn_Off : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb.useGravity = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
