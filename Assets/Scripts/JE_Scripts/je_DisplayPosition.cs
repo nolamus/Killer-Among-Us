cac8e3d2034130efb907767d1e3d87b37d249064
Joseph Ene <19315036+nolamus@users.noreply.github.com>
2020-06-16 00:14:20 -0400

    Initial Commit
    This is the first official commit of this branch

---
+++ Assets/Scripts/JE_Scripts/je_DisplayPosition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayPosition : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(transform.position);
+    }
+}
---
+++ Assets/Scripts/JE_Scripts/je_MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class je_MainMenu : MonoBehaviour
+{
+    //SceneTransition transitioner;
+    // Function called by main menu scene start button to transition to game scene
+    public void StartGame()
+    {
+        //transitioner.SaveScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene(1);
+    }
+
+    public void DisplaySettings()
+    {
+        SceneManager.LoadScene("je_Settings");
+    }
+    public void DisplayLevelSelect()
+    {
+        SceneManager.LoadScene("je_LevelSelect");
+        
+    }
+    public void DiplayMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+        //transitioner.ExitGame();
+    }
+}
