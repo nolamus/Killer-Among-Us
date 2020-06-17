450f7facd254ffdba822a8dcfde48b20babe2aa7
spalli235 <49597499+spalli235@users.noreply.github.com>
2020-06-17 13:38:51 -0400

    Added back in SP Dialogue Assets

---
+++ Assets/SP_Dialogue/Scripts/V2_Dialogue.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class V2_Dialogue
+{
+    // Name of NPCs we are dealing with
+    public string name;
+    // For loading into queue
+    public string[] sentences;
+}
