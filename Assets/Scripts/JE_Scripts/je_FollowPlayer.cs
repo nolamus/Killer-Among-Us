d2b19cb627d6f8f6a2b943126fd983d69e9bdb8c
Joseph Ene <19315036+nolamus@users.noreply.github.com>
2020-06-19 15:44:21 -0400

    Update Comments on JE_Scripts folder Scripts and delete je_PlayerCtrl

--- Assets/Scripts/JE_Scripts/je_FollowPlayer.cs
+++ Assets/Scripts/JE_Scripts/je_FollowPlayer.cs
@@ -1,14 +1,19 @@
-﻿using UnityEngine;
+﻿/*
+ * Project: Killer Among Us 
+ * This script was initially created by Joseph Ene
+ */
+// Forces the camera to update is position as the player is moving keeping the player within the camera's frame
+using UnityEngine;
 
 public class je_FollowPlayer : MonoBehaviour
 {
-    public Transform player;
+    public Transform player; // Binds to the Transform component of the player character game object
     public Vector3 offset;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.position + offset;
+        transform.position = player.position + offset; // Update the position of the object to the position of the player
         
     }
 }
