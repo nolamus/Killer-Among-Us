9d23d029bff47aff60f4a4e0d8937f50c5de82ea
Anika P <anika_0115@yahoo.com>
2020-07-01 15:19:57 -0400

    Item Scripts - in progress

---
+++ Assets/Scripts/AP_Scripts/PlayerPickup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// script for player and item interaction
+public class PlayerPickup : MonoBehaviour
+{
+    public int Activated;
+
+    public bool ItemPickedUp(GameObject obj)
+    {
+        switch (obj.tag)
+        {
+            // item for level 1
+            case "Broom": // tagged Broom
+                Activated++;
+                return true;
+
+            // scene trigger for level 1
+            case "Scene_LevelOne": // tagged Scene_LevelOne
+                SceneOne();
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
+    private void SceneOne()
+    {
+        //SceneManager.LoadScene(" ");
+    }
+
+}
