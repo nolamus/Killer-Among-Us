c6fedf877a94bbca63fe8f3c9125c876e0f445ef
Anika P <anika_0115@yahoo.com>
2020-07-07 14:12:12 -0400

    Player Pickup Script - done

--- Assets/Scripts/AP_Scripts/PlayerPickup.cs
+++ Assets/Scripts/AP_Scripts/PlayerPickup.cs
@@ -1,24 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement; // for shifting scenes
+using UnityEngine.UI; // for using UI element
 
+// backup script
+
+/*
 // script for player and item interaction
 public class PlayerPickup : MonoBehaviour
 {
-    public int Activated;
+    //public bool hasItem = false; // tracks if item was picked up
+    [SerializeField] private Image screenOverlay; // represents dirty screen overlay
+
+
+    public void Start()
+    {
+
+    }
 
     public bool ItemPickedUp(GameObject obj)
     {
-        switch (obj.tag)
+        switch (obj.tag) // cases represent tag of object
         {
             // item for level 1
-            case "Broom": // tagged Broom
-                Activated++;
+            case "Broom": 
+                //hasItem = true; // item obtained
+                return true;
+
+            // trigger for entering challenge with no item
+            case "Challenge1_noItem":
+                //hasItem = false;
+                //DirtyScreen(hasItem);
+                return true;
+
+            // trigger for entering challenge with item
+            case "Challenge1_Item":
+               // hasItem = true;
+                //DirtyScreen(hasItem);
                 return true;
 
             // scene trigger for level 1
-            case "Scene_LevelOne": // tagged Scene_LevelOne
+            case "Scene_LevelOne":
                 SceneOne();
                 return true;
 
@@ -32,4 +55,15 @@ public class PlayerPickup : MonoBehaviour
         //SceneManager.LoadScene(" ");
     }
 
+
+    public void DirtyScreen(bool val)
+    {
+        // if no item exists
+        if(val == false)
+        {
+            screenOverlay.enabled = true;
+        }
+
+    }
 }
+*/
