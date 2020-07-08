10621dfa684a2fd472d133f38dd4c098a3b92be5
Anika P <anika_0115@yahoo.com>
2020-07-08 17:31:22 -0400

    Adjusted item pickup script for level 1 and 2

--- Assets/Scripts/AP_Scripts/ItemPickupLvl2.cs
+++ Assets/Scripts/AP_Scripts/ItemPickupLvl2.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; // for shifting scenes
+using UnityEngine.UI; // for using UI element
 
-// script to attach to player for level 1
-public class ItemPickupLvl1 : MonoBehaviour
+// script to attach to player for level 2
+public class ItemPickup2 : MonoBehaviour
 {
+    [SerializeField] public Image screenOverlay; // represents dirty screen overlay
     public bool hasItem = false; // tracks if item was picked up
     // public AudioClip soundEffect;   // pickup sound effect
     // public GameObject pickupEffect; // pickup particle effect
@@ -13,7 +15,7 @@ public class ItemPickupLvl1 : MonoBehaviour
     void OnTriggerEnter2D(Collider2D item)
     {
         // if level item is picked up, destroy and record item obtained status
-        if (item.gameObject.CompareTag(" ")) // ** -----------> create tag for item, attach
+        if (item.gameObject.CompareTag("Broom"))
         {
             hasItem = true; // item obtained
 
@@ -23,31 +25,30 @@ public class ItemPickupLvl1 : MonoBehaviour
         }
 
         // challenge first checkpoint, player does not have item; initiate challenge
-        if (item.gameObject.CompareTag("Challenge1_noItem"))
+        if (item.gameObject.CompareTag("Challenge_noItem"))
         {
             // if player does not have item, start challenge
-            if(hasItem == false)
+            if (hasItem == false)
             {
-                // enable falling objects
-                // ** -----------> add code for commencing falling objects
-
-
+                // enable dirty screen challenge
+                screenOverlay.enabled = true;
+                // screenOverlay.SetActive(true;
             }
 
             Destroy(item.gameObject);
         }
 
         // challenge second checkpoint, challenge begins for item user; initiate challenge
-        if (item.gameObject.CompareTag("Challenge1_Item"))
+        if (item.gameObject.CompareTag("Challenge_Item"))
         {
-            // enable falling objects
-            // ** -----------> add code for commencing falling objects
+            // enable dirty screen challenge
+            screenOverlay.enabled = true;
 
             Destroy(item.gameObject);
         }
 
         // end of level reached, go to dialogue scene
-        if (item.gameObject.CompareTag("DialogueScene1")) // ** -----------> tag dialogue scene with this
+        if (item.gameObject.CompareTag("DialogueScene2")) // ** -----------> tag dialogue scene with this
         {
             // move onto dialogue scene
             //SceneManager.LoadScene(" "); // ** -----------> add dialogue scene name
@@ -56,5 +57,4 @@ public class ItemPickupLvl1 : MonoBehaviour
         }
 
     }
-
 }
