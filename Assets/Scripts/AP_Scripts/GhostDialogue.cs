72374b8fd02e07deeea2a017bbebdcb8f14975b3
Anika P <anika_0115@yahoo.com>
2020-07-08 13:28:50 -0400

    Ghost Dialogue Script created

---
+++ Assets/Scripts/AP_Scripts/GhostDialogue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostDialogue : MonoBehaviour
+{
+    // create a rectangular space to detect player approaching ghost
+    void OnTriggerEnter2D(Collider2D item)
+    {
+        if (item.gameObject.CompareTag("BackGhost"))
+        {
+            // trigger text to show up
+        }
+    }
+
+    // create a rectangular space to detect player leaving ghost
+    void OnTriggerExit2D(Collider2D item)
+    {
+        if (item.gameObject.CompareTag("BackGhost"))
+        {
+            // trigger text to turn off
+        }
+    }
+}
