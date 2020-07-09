8088b1942bb85facc1b1cfa6331bc4d82675b0f1
Joseph Ene <19315036+nolamus@users.noreply.github.com>
2020-07-09 07:52:04 -0400

    Modified Alec Player Script to disable player controls when Paused

---
+++ Assets/NoKeyDoorTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoKeyDoorTrigger : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("NoKeyDoor"))
+        {
+            Debug.Log("GameObject2 collided with " + col.name);
+        }
+    }
+}
