ded871baeff527776da6630909ee03024efbfc34
cela123 <66274837+cela123@users.noreply.github.com>
2020-07-16 16:11:48 -0400

    implementations of levels 3, 4, 5... removed unused sprites

---
+++ Assets/Scripts/AA_Scripts/Checkpoints.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoints : MonoBehaviour
+{
+    public Sprite redFlag;
+    public Sprite greenFlag;
+    private SpriteRenderer checkpointSpriteRenderer;
+    public bool checkpointReached;
+    private bool trigger;
+    // Use this for initialization
+    void Start()
+    {
+        checkpointSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
+    void Awake() { trigger = false; }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (!trigger)
+            if (collider.gameObject.layer == LayerMask.NameToLayer("Character"))
+                Trigger();
+    }
+
+    void Trigger()
+    {
+        checkpointSpriteRenderer.sprite = greenFlag;
+        checkpointReached = true;
+    }
+}
---
+++ Assets/Scripts/AA_Scripts/SinkingPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinkingPlatform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float fallDelay;
+    private Rigidbody2D platform;
+
+    void Start()
+    {
+        platform = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.CompareTag("Player"))
+            StartCoroutine(Fall());
+    }
+
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        platform.isKinematic = false;
+
+        yield return 0;
+    }
+
+}
