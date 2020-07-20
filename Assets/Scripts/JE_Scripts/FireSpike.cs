c1dfe1acffe66fa3113974d9961e2dec1431f3cd
cela123 <66274837+cela123@users.noreply.github.com>
2020-07-20 12:57:01 -0400

    commit

---
+++ Assets/Scripts/JE_Scripts/FireSpike.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSpike : MonoBehaviour
+{
+    public void Hazardous()
+    {
+        gameObject.layer = 18;
+    }
+
+    public void Safe()
+    {
+        gameObject.layer = 8;
+    }
+}
--- Assets/Scripts/TV_Scripts/Respawn.cs
+++ Assets/Scripts/TV_Scripts/Respawn.cs
@@ -8,6 +8,11 @@ public class Respawn : MonoBehaviour
     [SerializeField] private Transform respawnPoint;
     // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D other){
-      player.transform.position = respawnPoint.transform.position;
+        if(other.name == "boy" )
+        {
+            player.transform.position = respawnPoint.transform.position;
+        }
+        
+      
     }
 }
