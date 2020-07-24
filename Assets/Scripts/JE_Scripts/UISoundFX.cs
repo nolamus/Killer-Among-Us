7940d6c81532bad9b5df02ee53482b3d861e1ca1
Joseph Ene <19315036+nolamus@users.noreply.github.com>
2020-07-24 14:25:03 -0400

    Added Sound FX for UI buttons

---
+++ Assets/Scripts/JE_Scripts/UISoundFX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISoundFX : MonoBehaviour
+{
+    public AudioClip soundfx;
+    private Button uibutton { get { return GetComponent<Button>(); } }
+    private AudioSource uiSoundSource { get { return GetComponent<AudioSource>(); } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+        uiSoundSource.clip = soundfx;
+        uiSoundSource.playOnAwake = false;
+
+        uibutton.onClick.AddListener(() => SoundEffect());
+    }
+
+    void SoundEffect()
+    {
+        uiSoundSource.PlayOneShot(soundfx);
+    }
+}
